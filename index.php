095cfa738c9efccfbe96a4b833fc3459e3be6422
Mauricio Mathias <mauricioneis15@gmail.com>
2019-10-17 16:49:42 -0300

    Correção de bug

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 		</a>
 	</div>
 	<div class="col-xs-6 col-sm-3 col-md-2">
-		<a href="customers" class="btn btn-default">
+		<a href="customers/index.php" class="btn btn-default">
 			<div class="row">
 				<div class="col-xs-12 text-center">
 					<i class="fa fa-user fa-5x"></i>
